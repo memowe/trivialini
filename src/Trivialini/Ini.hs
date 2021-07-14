d44443d607f92ac199284237585d1a51a6e78995
Mirko Westermeier <mirko@westermeier.de>
2021-07-14 10:53:28 +0200

    Combine Ini rendering functions

--- src/Trivialini/Ini.hs
+++ src/Trivialini/Ini.hs
@@ -6,12 +6,11 @@ import Data.Map ( assocs, Map )
 -- | data structure for "ini" file representation
 type Ini = Map String (Map String String)
 
-_showMap :: Map String String -> String
-_showMap = unlines . map line . assocs
-    where line (k, v) = k ++ " = " ++ v
-
 -- | Nice string representation of Ini data.
 --   The output parses as Ini data.
 showIni :: Ini -> String
 showIni = unlines . map section . assocs
-    where section (name, sec) = "[" ++ name ++ "]\n" ++ _showMap sec
+    where
+        section (name, sec) = "[" ++ name ++ "]\n" ++ pairs sec
+        pairs               = unlines . map pair . assocs
+        pair (k, v)         = k ++ " = " ++ v
