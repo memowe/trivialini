61010e610335d0659266319d7d3f28fccaffa985
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2021-09-02 14:54:39 +0200

    Inline Ini type in main interface

--- src/Trivialini.hs
+++ src/Trivialini.hs
@@ -12,9 +12,14 @@ module Trivialini
   -- * Ini files and data
   -- $intro
     readIniFile
+  -- * Ini data is a Map of Maps
+  , Ini(..)
   ) where
 
-import Trivialini.Ini ( Ini(..), showIni )
+import Data.Map ( assocs, Map, fromList )
+import Data.List ( dropWhileEnd )
+import Text.ParserCombinators.ReadP
+    ( between, char, many, munch1, readP_to_S, skipMany1 )
 
 {- $intro
 Consider a simple ini file @config.ini@ like this:
@@ -33,11 +38,40 @@ and @"something else"@. These sections contain a dictionary of strings each,
 the keys being some string followed by @"="@, and anything else until end of
 the line as values. The leading and trailing spaces in section headers, keys
 and values are trimmed.
-
-"Trivialini" parses this data as `Ini`, which is simply a 'Data.Map.Map' of
-'Data.Map.Map's of 'String's:
 -}
 
 -- | Read 'Ini' data from a given filename
 readIniFile :: FilePath -> IO Ini
-readIniFile file = readIni <$> readFile file
+readIniFile file = read <$> readFile file
+
+-- | As ini files consist of sections with a name, each with a list of
+-- key-value pairs, A "two-dimensional" 'Map' of 'String's seems to be very
+-- natural.
+newtype Ini = Ini { pairMap :: Map String (Map String String) }
+  deriving
+    ( Eq -- ^ Default Eq instance
+    )
+
+-- | Stringification of 'Ini' data. The result can be parsed again as 'Ini'
+-- data.
+instance Show Ini where
+  show = unlines . map section . assocs . pairMap
+    where section (name, sec) = "[" ++ name ++ "]\n" ++ pairs sec
+          pairs               = unlines . map pair . assocs
+          pair (k, v)         = k ++ " = " ++ show v
+
+-- | Parsing of Ini strings.
+instance Read Ini where
+  readsPrec _ = readP_to_S parser
+    where parser  = Ini . fromList <$> many section
+          section = do
+            name  <- trim <$> between (char '[') (char ']' >> nls) (no "=\n]")
+            pairs <- many pair
+            return (name, fromList pairs)
+          pair    = do
+            key <- trim <$> no "\n[="
+            val <- trim <$> between (char '=') nls (no "\n")
+            return (key, val)
+          nls     = skipMany1 (char '\n')
+          no      = munch1 . flip notElem
+          trim    = dropWhile (==' ') . dropWhileEnd (==' ')
